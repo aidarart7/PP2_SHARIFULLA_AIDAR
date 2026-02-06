b1e4161fdcd61eeba8f20a362f47bc7cd9e94655
aidarart7 <20aidar07@gmail.com>
2026-02-06 23:09:43 +0500

    Add Practice2 - boolean

---
+++ Practice 2/boolean/boolean_operators.py
@@ -0,0 +1,14 @@
+class myclass():
+  def __len__(self):
+    return 0
+
+myobj = myclass()
+print(bool(myobj))
+
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
